1376e7cc59bb7dcd1a1708dcbbb52c7893776592
M-Hasnain-Raza <razamhasnain39@gmail.com>
2025-01-19 10:59:39 +0500

    add sanitySchema fix pdf

---
+++ sanitySchema.js
@@ -0,0 +1,10 @@
+export default {
+    name: "product",
+    type: "document",
+    fields: [
+        {name: "name", type: "string", title: "Cheese burger"},
+        {name: "price", type: "number", title: "Price"},
+        {name: "quantity", type: "number", title: "Quantity"},
+
+    ]
+}
